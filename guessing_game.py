a49465db1689d321e3430e9f346740332eaa7328
Madhumitha19092006 <madhumithamadhumitha984@gmail.com>
2026-05-11 15:30:29 +0530

    Create guessing_game.py

---
+++ guessing_game.py
@@ -0,0 +1,11 @@
+import random
+
+number = random.randint(1, 10)
+
+guess = int(input('Guess a number between 1 and 10: '))
+
+if guess == number:
+    print('Correct Guess')
+else:
+    print('Wrong Guess')
+    print('Correct Number is', number)
